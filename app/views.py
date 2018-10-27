adc9dafed467252bd0524eeccf96c9a2151e8c2f
Cuong Nguyen <cuongpianna1996@gmail.com>
2018-10-27 14:23:22 +0700

    login1

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
+@login_required
 def hello_world():
     user = {'username': 'Cuong'}
     return render_template('index.html', title='Home', user=user)
@@ -11,8 +14,21 @@ def hello_world():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('/'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        print(user.password_hash)
+        if user is None:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('/'))
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for(''))
